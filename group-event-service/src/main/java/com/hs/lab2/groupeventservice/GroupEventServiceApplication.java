6901340328f4ad9ef7551108b0669d1c884f625e
dazavv <dasha7348@gmail.com>
2025-10-08 00:17:21 +0300

    chore: добавлен group-event-service

---
+++ group-event-service/src/main/java/com/hs/lab2/groupeventservice/GroupEventServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hs.lab2.groupeventservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupEventServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupEventServiceApplication.class, args);
+    }
+
+}
