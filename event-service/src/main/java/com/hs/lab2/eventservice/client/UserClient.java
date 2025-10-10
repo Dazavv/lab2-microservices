790f1b89fa1d3e31d14c8af83a5564067ed29b5c
dazavv <dasha7348@gmail.com>
2025-10-10 21:46:38 +0300

    chore: добавлен Feign Client для user-service

---
+++ event-service/src/main/java/com/hs/lab2/eventservice/client/UserClient.java
@@ -0,0 +1,14 @@
+package com.hs.lab2.eventservice.client;
+
+import com.hs.lab2.eventservice.dto.responses.UserDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import reactivefeign.spring.config.ReactiveFeignClient;
+import reactor.core.publisher.Mono;
+
+@ReactiveFeignClient(name = "user-service")
+public interface UserClient {
+
+    @GetMapping("/api/v1/users/{id}")
+    Mono<UserDto> getUserById(@PathVariable("id") Long id);
+}
